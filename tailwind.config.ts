144fdd48948b70041e992a1f41002f88f9ad168f
micci184 <micci184@gmail.com>
2024-11-24 21:12:20 +0900

    create counter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,45 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        main900: "#464E53", // 濃いグレー
+        main400: "#CCCCCC", // 中間グレー
+        main200: "#E2E8F0", // 明るいグレー
+        white: "#FFFFFF", // 白
+        accent: "#FDBBD7", // ピンク
+      },
+      fontFamily: {
+        header: ['"Concert One"', "sans-serif"],
+        nav: ['"Concert One"', "sans-serif"],
+        buttonLarge: ['"Poppins"', "sans-serif"],
+        tag: ['"Concert One"', "sans-serif"],
+        body: ['"Poppins"', "sans-serif"],
+      },
+      fontSize: {
+        body: "1.2rem",
+        tagline: "2rem",
+        taglineSub: "1.5rem",
+        heading: "2.5rem",
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: "text-main900",
+            h1: {
+              textAlign: "center",
+              color: "text-main900",
+            },
+            h2: {
+              color: "text-main900",
+            },
+            h3: {
+              color: "text-main900",
+            },
+            p: {
+              color: "text-main900",
+              marginBottom: "1.0rem",
+            },
+          },
+        },
       },
     },
   },
